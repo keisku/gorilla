dca24095dc2cdb2f37a75ff2ee69837a97610a2b
kei6u <keisuke.umegaki.630@gmail.com>
2021-10-11 07:41:49 +0900

    refactor bit_writer

--- bit_writer.go
+++ bit_writer.go
@@ -6,9 +6,9 @@ import (
 )
 
 type bitWriter struct {
-	w     io.Writer
-	b     [1]byte
-	count uint8
+	w      io.Writer
+	buffer byte
+	count  uint8
 }
 
 // newBitWriter returns a writer that buffers bits and write the resulting bytes to 'w'
@@ -18,26 +18,26 @@ func newBitWriter(w io.Writer) *bitWriter {
 	}
 }
 
-// writeBit writes a single bit to the stream, writing a new byte to 'w' if required.
+// writeBit writes a single bit.
 func (b *bitWriter) writeBit(bit bit) error {
 	if bit {
-		b.b[0] |= 1 << (b.count - 1)
+		b.buffer |= 1 << (b.count - 1)
 	}
 
 	b.count--
 
 	if b.count == 0 {
-		if _, err := b.w.Write(b.b[:]); err != nil {
+		if _, err := b.w.Write([]byte{b.buffer}); err != nil {
 			return fmt.Errorf("failed to write a bit: %w", err)
 		}
-		b.b[0] = 0
+		b.buffer = 0
 		b.count = 8
 	}
 
 	return nil
 }
 
-// writeBits writes the nbits least significant bits of u, most-significant-bit first.
+// writeBits writes the nbits least significant bits of u64, most-significant-bit first.
 func (b *bitWriter) writeBits(u64 uint64, nbits int) error {
 	u64 <<= (64 - uint(nbits))
 	for nbits >= 8 {
@@ -64,21 +64,21 @@ func (b *bitWriter) writeBits(u64 uint64, nbits int) error {
 
 // writeByte writes a single byte to the stream, regardless of alignment
 func (b *bitWriter) writeByte(byt byte) error {
-	// fill up b.b with b.count bits from byt
-	b.b[0] |= byt >> (8 - b.count)
+	// fill up b.buffer with b.count bits from byt
+	b.buffer |= byt >> (8 - b.count)
 
-	if _, err := b.w.Write(b.b[:]); err != nil {
+	if _, err := b.w.Write([]byte{b.buffer}); err != nil {
 		return fmt.Errorf("failed to write a byte: %w", err)
 	}
-	b.b[0] = byt << b.count
+	b.buffer = byt << b.count
 
 	return nil
 }
 
 // flush empties the currently in-process byte by filling it with 'bit'.
-func (w *bitWriter) flush(bit bit) error {
-	for w.count != 8 {
-		err := w.writeBit(bit)
+func (b *bitWriter) flush(bit bit) error {
+	for b.count != 8 {
+		err := b.writeBit(bit)
 		if err != nil {
 			return err
 		}
